26f488abd5d124a318286f5a708d16c4df949827
Stephie <stephaniecrocker91@gmail.com>
2023-03-31 21:19:49 +0000

    Create custom permissions IsEditorInChiefOrReadOnly and IsEditorOrReadOnly

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -11,3 +11,15 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user.role == 'SUPERADMIN'
+
+class IsEditorInChiefOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.role == 'EDITORINCHIEF'
+
+class IsEditorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.role == 'EDITOR'
--- roles/views.py
+++ roles/views.py
@@ -6,24 +6,24 @@ from .models import Role
 from .serializers import RoleSerializer
 from drf_api.permissions import IsAdminOrReadOnly
 
+
 class RoleList(APIView):
-## Get method: fetch's all profile objects, serializes and returns in Response.
+    ## Get method: fetch's all profile objects, serializes and returns in Response.
     def get(self, request):
         roles = Role.objects.all()
-        serializer = RoleSerializer(
-            roles,
-            many=True,
-            context={'request': request}
-            )
+        serializer = RoleSerializer(roles, many=True, context={"request": request})
         return Response(serializer.data)
+
 
 class RoleDetail(APIView):
     """
     Retrieve or update a profile if you're the owner.
     """
+
     # setting serializer class attribute in this view, the rest framework with automatically render the form based on our serializer fields, as opposed to raw Json.
     serializer_class = RoleSerializer
     permission_classes = [IsAdminOrReadOnly]
+
     def get_object(self, pk):
         try:
             role = Role.objects.get(pk=pk)
@@ -32,28 +32,43 @@ class RoleDetail(APIView):
         except Role.DoesNotExist:
             raise Http404
 
-## Get method fetching profile detail using pk.
+    ## Put Method: Updating data.
+    # Fetches profile, calls serializer and saves if serializer is valid.
+    ## Else, 400_BAD_REQUEST
+
+    def put(self, request, pk):
+        role = self.get_object(pk)
+        serializer = RoleSerializer(
+            role, data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class RoleUserDetail(APIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+
+    # setting serializer class attribute in this view, the rest framework with automatically render the form based on our serializer fields, as opposed to raw Json.
+    serializer_class = RoleSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            role = Role.objects.get(pk=pk)
+            self.check_object_permissions(self.request, role)
+            return role
+        except Role.DoesNotExist:
+            raise Http404
+
+    ## Get method fetching profile detail using pk.
     def get(self, request, pk):
         role = self.get_object(pk)
         serializer = RoleSerializer(
             role,
             # As the logged in user is part of the request object, we need to pass it as context object when we call our serializers in our views.
-            context={'request': request}
-            )
+            context={"request": request},
+        )
         return Response(serializer.data)
-
-## Put Method: Updating data. 
-# Fetches profile, calls serializer and saves if serializer is valid.
-## Else, 400_BAD_REQUEST
-    
-    def put(self, request, pk):
-        role = self.get_object(pk)
-        serializer = RoleSerializer(
-            role,
-            data=request.data,
-            context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
