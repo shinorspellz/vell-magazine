6527a47f22416122cfef606c198b6280fc271944
ALASHI1 <115797122+Muyiwa0@users.noreply.github.com>
2023-03-31 21:59:23 +0100

    roles fix

--- roles/urls.py
+++ roles/urls.py
@@ -4,4 +4,5 @@ from roles import views
 urlpatterns = [
     path("roles/", views.RoleList.as_view()),
     path("roles/<int:pk>/", views.RoleDetail.as_view()),
+    path("roles/<int:pk>/", views.RoleUserDetail.as_view()),
 ]
