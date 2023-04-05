795a7a68783f65e09de6203565dca020f7e1533f
anusinghmsc <124518815+anusinghmsc@users.noreply.github.com>
2023-04-05 21:43:51 +0100

    Update permissions.py

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -17,21 +17,3 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         ):
             return True
         return request.user.role == "SUPERADMIN"
-"""
-class IsEditorInChiefOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        print(request.user.role.role)
-        if (
-            request.method in permissions.SAFE_METHODS
-            and request.user.role.role == "EDITORINCHIEF"
-        ):
-            return True
-        return request.user.role == "EDITORINCHIEF"
-
-
-class IsEditorOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user.role == 'EDITOR'
-"""
